51ed0b127de7f565412d217273b9ab2c8cf5ce58
orcsab <orcsab@gmail.com>
2014-04-27 16:38:47 +0800

    Form submission appears to be working.  Not properly posting the data on the server side yet.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -8,33 +8,50 @@ angular.module('starter.controllers', [])
 
     console.log('listStoredImages(): ' + Server.name + '/getPhotos');
     $http({method: 'GET',
-           url: 'http://' + Server.name + '/getPhotos'}).
+           url: Server.name + '/getPhotos'}).
       success(function(data, status, headers, config) {
         console.log('listStoredImages(): successful GET');
         $scope.photoMetaList = angular.fromJson(data);
       }).
       error(function(data, status, headers, config) {
         console.log('listStoredImages(): failed GET');
-        $scope.photoMetaList = ['fail'];
       });
 
   }
 })
 
-.controller('SnapCtrl', function($scope) {
+.controller('SnapCtrl', function($scope, $http) {
+  $scope.photo = {
+    user: "unnamed",
+    location: "unspecified",
+    caption: "uncompleted"
+  }
   $scope.takePic = function() {
     console.log('In takePic()');
     capturePhoto();
   }
-  $scope.submitPhoto = function() {
+  $scope.submitPhoto = function(photo) {
     console.log('in submitPhoto()');
-    var photo = new Photo();
+    var photo = {
+      meta: {
+        creator: $scope.photo.name,
+        date: new Date(),
+        caption: $scope.photo.caption,
+        location: $scope.photo.location
+      }
+      // data: document.getElementById('smallImage').src
+    };
 
-    photo.meta.creator = document.getElementById('name').src;
-    photo.meta.date = document.getElementById('date').src;
-    photo.meta.caption = document.getElementById('caption').src;
-    photo.meta.location = document.getElementById('location').src;
-    photo.data = document.getElementById('smallImage').src;
+    console.log('submitPhoto(): posting ' + JSON.stringify(photo));
+    $http({
+      url: Server.name + '/postPhoto',
+      method: "POST",
+      data: photo.meta
+    }).success(function(data, status, headers, config) {
+      $scope.data = data;
+    }).error(function(data, status, headers, config) {
+      $scope.status = status;
+    });
 
   }
 })
--- www/js/server.js
+++ www/js/server.js
@@ -1,26 +1,5 @@
 var Server = {
-  name: 'pure-sands-5851.herokuapp.com',
-  port: 80,
+  // name: 'pure-sands-5851.herokuapp.com',
+  name: 'http://localhost:4791',
   awsBucket: 'mipitw',
-  sendPhoto: function (data, creator, caption, date, location) {
-  },
-
-  // get a list of photos, optionally owned/created by "creator"
-  // the "id" if the ID of the HTML element to which the results will
-  // be placed
-  getPhotoList: function (id, creator) {
-    creator = (typeof creator === "undefined") ? "" : creator;
-
-    console.log('in getPhotoList()');
-    $http({method: 'GET', url: this.name + '/getPhotos'}).
-      success(function(data, status, headers, config) {
-        document.getElementById(id).innerHTML = data;
-      }).
-      error(function(data, status, headers, config) {
-        document.getElementById(id).innerHTML = "failed to get photo meta from " + name;
-      });
-  },
-
-  getPhoto: function (id) {
-  }
 }
