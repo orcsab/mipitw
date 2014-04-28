32d24daf2279aa82364cb29db787fc2805345413
orcsab <orcsab@gmail.com>
2014-04-28 20:04:13 +0800

    Fixed URL in now-pointless Server class.

--- www/js/server.js
+++ www/js/server.js
@@ -1,5 +1,4 @@
 var Server = {
-  // name: 'pure-sands-5851.herokuapp.com',
-  name: 'http://localhost:4791',
-  awsBucket: 'mipitw',
+  name: 'http://pure-sands-5851.herokuapp.com'
+  // name: 'http://localhost:4791'
 }
